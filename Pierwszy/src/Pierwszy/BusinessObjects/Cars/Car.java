9ebcead15c855e0f06c3f166556e9db6ae257700
student <student@student-vm>
2012-10-03 11:36:12 +0200

    First Commit

---
+++ Pierwszy/src/Pierwszy/BusinessObjects/Cars/Car.java
@@ -0,0 +1,47 @@
+package Pierwszy.BusinessObjects.Cars;
+
+public class Car {
+
+	//pola statyczne
+	
+	//pola prywatne
+		private String mark;
+		private String regnumber;
+	
+	//pola publiczne X
+	
+	//konstruktory
+		public Car(String mark, String regnumber)
+		{
+			this.mark = mark;
+			this.regnumber = mark;
+		}
+		
+		public Car()
+		{
+			this("","");
+		}
+		
+	//właściwości
+		public String getMark() {
+			return mark;
+		}
+
+		public void setMark(String mark) {
+			this.mark = mark;
+		}
+
+		public String getRegnumber() {
+			return regnumber;
+		}
+
+		public void setRegnumber(String regnumber) {
+			this.regnumber = regnumber;
+		}
+		
+	//metody
+		public void printData()
+		{
+			System.out.println("marka: "+mark);
+		}
+}
